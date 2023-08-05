5a3f7379ee982b8b0453e0523ce695cff0f3508e
oralxan <141113633+oralxan@users.noreply.github.com>
2023-08-05 21:37:35 +0500

    Add files via upload
    instagram turtle

---
+++ instagram.py
@@ -0,0 +1,46 @@
+#instagram
+from turtle import*
+bgcolor('#ff0066')
+pencolor('white')
+width(23)
+penup()
+goto(160,-100)
+pendown()
+
+left(90)
+for i in range(4):
+    forward(250)
+    circle(34,90)
+penup()
+goto(85,30)
+pendown()
+circle(80,360)
+penup()
+goto(110,130)
+pendown()
+circle(7,360)
+#window
+
+speed(2)
+bgcolor("black")
+penup()
+goto(-50,60)
+pendown()
+color("#00adef")
+begin_fill()
+goto(100,100)
+goto(100,-100)
+goto(-50,-60)
+goto(-50,60)
+
+end_fill()
+color("black")
+
+goto(15,100)
+color("black")
+width(10)
+goto(15,-100)
+penup()
+goto(100,0)
+pendown()
+goto(-100,0)
